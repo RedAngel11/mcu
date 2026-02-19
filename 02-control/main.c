c17eb3bacce45d9efac243f8b6212c14cd48e76d
Ksusha Titova <ksushat75@gmail.com>
2026-02-19 16:21:05 +0300

    echo succes

---
+++ 02-control/main.c
@@ -0,0 +1,17 @@
+#include "pico/stdlib.h" 
+#include "hardware/gpio.h"
+#include "stdio-task/stdio-task.h"
+
+const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+stdio_task_init;
+
+int main()
+{
+    stdio_init_all(); 
+    
+    while (1)
+    {
+
+        
+    }
+}
--- 02-echo/main.c
+++ 02-echo/main.c
@@ -1,16 +1,19 @@
+#include "stdio.h"
 #include "pico/stdlib.h" 
 #include "hardware/gpio.h"
+
 
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
 int main()
 {
     stdio_init_all(); 
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
     
     while (1)
     {
-        gpio_init(LED_PIN);
-        gpio_set_dir(LED_PIN, GPIO_OUT);
+
         char symbol = getchar(); 
         
         printf("received char: %c [ ASCII code: %d ]\n", symbol, symbol);
@@ -20,7 +23,12 @@ int main()
             gpio_put(LED_PIN, true);
             printf("led enable done\n");
             break;
-
+        case 'd':
+            gpio_put(LED_PIN, false);
+            printf("led disable done\n");
+            break;
+        case 'v':
+            printf("конечно же новейшая версия\n");
         default:
             break;
         }
